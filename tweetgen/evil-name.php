b9a5163cf0287e495593e55886aa2bcd66ade5df
elliotfiske <elliotfiske@gmail.com>
2015-08-12 09:18:18 -0700

    Added subtitle

--- tweetgen/evil-name.php
+++ tweetgen/evil-name.php
@@ -6,6 +6,7 @@ function printRandomItem($array) {
     echo $array[array_rand($array)]; 
 }
 
+
 $firstname1 = array(
 "Evil",
 "Nasty",
@@ -16,7 +17,6 @@ $firstname1 = array(
 "Bob",
 "Devil",
 "Ugly",
-"Dark",
 "Twist",
 "Foul",
 
@@ -32,6 +32,7 @@ $firstname2 = array(
 "blade",
 "face",
 "mouth",
+"skull",
 
 
 
@@ -39,14 +40,12 @@ $firstname2 = array(
 
 $lastname1 = array(
 "Gold",
-"Darker",
 "Mangle",
 "Cruel",
 "Horror",
 "Stink",
 "Fart",
 "Mafia",
-
 
 );
 
@@ -60,6 +59,70 @@ $lastname2 = array(
 "demon",
 "catcher",
 "gangster",
+"death",
+
+);
+
+$friendWords = array(
+"KING",
+"QUEEN",
+"LORD",
+"FATHER",
+"MOTHER",
+"FRIEND",
+"LEADER",
+"SPAWN",
+"SCOURGE",
+"CHILD",
+
+);
+
+$badThings = array(
+"DARKNESS",
+"EVIL",
+"SIN",
+"HATRED",
+"THE HIVE",
+"DEMONS",
+"THE DEEP",
+"AWKWARD CONVERSATIONS",
+
+);
+
+$enemyWords = array(
+"FOE",
+"SCOURGE",
+"TERROR",
+"HORROR",
+"DEMON",
+
+);
+
+$places = array(
+"SPAIN",
+"THE VILLAGE",
+"THE KINGDOM",
+"MY BUTT",
+"TRANSYLVANIA",
+
+);
+
+$descriptiveAdjective = array(
+"THE RED",
+"THE ORANGE",
+"CREEPING",
+"THE BLUE",
+
+
+);
+
+$badNoun = array(
+"MENACE",
+"DEATH",
+"TERROR",
+"DEMON",
+"DOOM",
+"DARKNESS",
 
 );
 
@@ -69,6 +132,29 @@ echo ' ';
 printRandomItem($lastname1);
 printRandomItem($lastname2);
 
+echo ',';
+
 echo PHP_EOL;
+
+$whuh = rand(0, 2);
+if ($whuh == 0) {
+    // <Ally> of <Bad thing>    
+    printRandomItem($friendWords);
+    echo ' OF ';
+    printRandomItem($badThings);
+}
+else if ($whuh == 1) {
+    // <Noun> of <Place>
+    printRandomItem($enemyWords);
+    echo ' OF ';
+    printRandomItem($places);
+}
+else {
+    // The <Adjective> <Noun>
+    printRandomItem($descriptiveAdjective);
+    printRandomItem($badNoun);
+}
+
+echp PHP_EOL;
 
 ?>
